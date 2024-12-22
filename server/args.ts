6d3f5099e0d2858c0827e9ad7658aac5e65f1764
Woidly <woidly@proton.me>
2024-12-22 11:55:59 +0900

    fix(server): remove old default argument values

--- server/args.ts
+++ server/args.ts
@@ -24,36 +24,19 @@ export default async function parse(): Promise<Args> {
     },
   });
   return {
-    dictionary: await checkOrDefault(
-      values.dictionary,
-      ["../dictionary.txt", "dictionary.txt", "../../dictionary.txt"],
-      "dictionary"
-    ),
+    dictionary: await checkPath(values.dictionary, "dictionary"),
     port: parseInt(values.port || "27974") || 27974,
-    solver: await checkOrDefault(
-      values.solver,
-      [
-        "../target/release/spellcast-solver",
-        "../../target/release/spellcast-solver",
-        "target/release/spellcast-solver",
-        "spellcast-solver",
-      ],
-      "solver"
-    ),
+    solver: await checkPath(values.solver, "solver"),
   };
 }
 
-async function checkOrDefault(path: string | undefined, defaults: string[], what: string): Promise<string> {
+async function checkPath(path: string | undefined, what: string): Promise<string> {
   if (path) {
     if (await Bun.file(path).exists()) return path;
     console.error("Invalid path specified for", what);
     process.exit(1);
+  } else {
+    console.error("Path is not specified for", what);
+    process.exit(1);
   }
-  for (let def of defaults) {
-    if (await Bun.file(def).exists()) {
-      return def;
-    }
-  }
-  console.error("Failed to get path for", what);
-  process.exit(1);
 }
